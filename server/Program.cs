8a3d0d279791f6890d7dbe91e975d6c08b17f5ac
iSlammedMyKindle <108752129+iSlammedMyKindle@users.noreply.github.com>
2022-11-28 23:18:51 -0500

    joysticks are alive!

--- server/Program.cs
+++ server/Program.cs
@@ -10,8 +10,10 @@ var controller = new ViGEmClient().CreateXbox360Controller();
 Dictionary<byte, Xbox360Property> analogMap = new Dictionary<byte, Xbox360Property>{
     {15, Xbox360Axis.LeftThumbX},
     {16, Xbox360Axis.LeftThumbY},
-    {17, Xbox360Slider.LeftTrigger},
-    {18, Xbox360Slider.RightTrigger}
+    {17, Xbox360Axis.RightThumbX},
+    {18, Xbox360Axis.RightThumbY},
+    {19, Xbox360Slider.LeftTrigger},
+    {20, Xbox360Slider.RightTrigger}
 };
 
 server.Start(socket =>{
@@ -28,6 +30,7 @@ server.Start(socket =>{
 
     socket.OnClose = ()=>{
         controller.Disconnect();
+        Console.WriteLine("Disconnected");
     };
 
     //This webSocket library does the Lord's work and automatically detects and parses strings :D
@@ -35,17 +38,25 @@ server.Start(socket =>{
         //So far the only thing we're using this for is for joystick movement, there's no way to send the joystick strength in bytes without heavily compressing information.
         var resArray = JsonDocument.Parse(message);
 
-        if(resArray.RootElement.ValueKind == JsonValueKind.Array)
+        Console.WriteLine("JS " + message);
+
+        if(resArray.RootElement.ValueKind == JsonValueKind.Array){
+            //Joystick X
             controller.SetAxisValue((Xbox360Axis) analogMap[(byte)resArray.RootElement[0].GetInt16()], 
-            (byte)resArray.RootElement[1].GetInt16());
+            (short)resArray.RootElement[1].GetInt16());
+
+            //Joystick Y
+            controller.SetAxisValue((Xbox360Axis) analogMap[(byte)(resArray.RootElement[0].GetInt16() + 1)], 
+            (short)resArray.RootElement[2].GetInt16());
+        }
     };
 
     //Non-strings go here, most inputs will come down this way
     socket.OnBinary = message=>{
         Console.WriteLine(message[0].ToString() + ' ' + message[1].ToString() + ' ' + message[2].ToString());
 
-        if(message[1] < 15) controller.SetButtonState(message[1], message[2] == 255);
-        else if(message[1] == 17 || message[1] == 18) controller.SetSliderValue((Xbox360Slider)analogMap[message[1]], message[2]);
+        if(message[1] < 18) controller.SetButtonState(message[1], message[2] == 255);
+        else if(message[1] == 19 || message[1] == 20) controller.SetSliderValue((Xbox360Slider)analogMap[message[1]], message[2]);
     };
 });
 
