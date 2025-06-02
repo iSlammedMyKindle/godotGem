54b8ccfb75d1a197f7db4a7d5dd85272a84914fe
iSlammedMyKindle <108752129+iSlammedMyKindle@users.noreply.github.com>
2025-06-02 08:29:44 -0400

    Bg no lonver overheats my laptop

--- client/bg/bg.gd
+++ client/bg/bg.gd
@@ -18,11 +18,11 @@ func _ready():
 	bgAnim()
 
 func bgAnim():
-	
 	var tween = create_tween()
-	tween.set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC).set_loops()
+	tween.set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
 	tween.tween_property($background, "modulate", colors[colorIndex], 10)
-	tween.tween_callback(bgAnim).set_delay(10)
+	tween.tween_callback(bgAnim)
+	tween.play()
 	
 	colorIndex += 1
 	if colorIndex > colors.size()-1: colorIndex = 0
