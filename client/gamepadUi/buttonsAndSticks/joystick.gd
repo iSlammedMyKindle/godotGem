f0746e37e926244e9aca57a798e6e060520dcb10
iSlammedMyKindle <108752129+iSlammedMyKindle@users.noreply.github.com>
2025-08-02 20:50:32 -0400

    Joystick sounds!

--- client/gamepadUi/buttonsAndSticks/joystick.gd
+++ client/gamepadUi/buttonsAndSticks/joystick.gd
@@ -1,12 +1,29 @@
 extends Node2D
 var maxDistance = 75
 var prefix = ''
+var prev = 0
+var volume = 0
+var save: Node
+var buttonSounds = false
 
 func _ready():
 	prefix = 'L' if name == 'SL' else 'R'
 	add_to_group('S' + prefix)
+	add_to_group('save')
 
-func _process(_delta: float):
+# Boilerplate code I stole from gamepadUi.gd
+func receive_save(node, _status):
+	save = node
+	buttonSounds = save.get_val('general', 'buttonsounds', false)
+	
+func update_save_val(sec, key, val):
+	if sec == 'general' and key == 'buttonsounds':
+		buttonSounds = val
+# End save boilerplate
+
+func _process(delta: float):
+	
+	# Create X & Y values to represent a traditional value (from -1 to 1)
 	var positiveX = Input.get_action_strength( prefix + 'StickR') > 0
 	var positiveY = Input.get_action_strength( prefix + 'StickD') > 0
 	var x = Input.get_action_strength( prefix + 'StickR' if positiveX else prefix + 'StickL')
@@ -19,6 +36,35 @@ func _process(_delta: float):
 	
 	$container/bottom.position = Vector2(maxDistance * x, maxDistance * y)
 	$container/bottom/top.position = Vector2(maxDistance * x * .12, maxDistance * y * .12)
+	
+	# Sound - The joysticks will have a "wooshing" effect like in tetris worlds. It'll be fluid - depending on where the joystick is in conjunction with the entire space, the volume will adjust
+	if not buttonSounds: return
+
+	var newVal = snappedf((x+y), 0.1)
+	if newVal != prev:
+		# Perform some subtraction on the largest and smallest numbers, then raise the volume by that amount
+		var diff = abs(newVal) - abs(prev) if abs(newVal) > abs(prev) else abs(prev) - abs(newVal)
+		volume = volume + diff
+		#print(diff)
+	
+	if volume > .25:
+		volume = .25
+	if volume != 0:
+		volume = volume - (1.25 * delta)
+		#print(volume)
+	if volume < 0: volume = 0
+	
+	
+	$analogSfx.volume_linear = volume
+	# To protect my ears against bad numbers (during development it blasted to 2000DB... not fun)
+	if $analogSfx.volume_linear > 2:
+		$analogSfx.stop()
+	#$analogSfx.volume_linear > 0 and
+	elif not $analogSfx.playing:
+		$analogSfx.play()
+
+	if prev != newVal:
+		prev = newVal
 
 func press(_stick):
 	$AnimationPlayer.stop()
